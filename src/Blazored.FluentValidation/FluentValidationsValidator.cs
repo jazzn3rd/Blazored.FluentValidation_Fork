bd13e94c5c92bcc3a9828bd5281552b8e6c2ee7d
Rikard Skoog <rikard.skoog@iosoft.se>
2021-06-12 11:47:02 +0200

    Added Fix for RuleSets

--- src/Blazored.FluentValidation/FluentValidationsValidator.cs
+++ src/Blazored.FluentValidation/FluentValidationsValidator.cs
@@ -1,8 +1,8 @@
-﻿using FluentValidation;
+﻿using System;
+using FluentValidation;
 using FluentValidation.Internal;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
-using System;
 
 namespace Blazored.FluentValidation
 {
@@ -15,11 +15,12 @@ namespace Blazored.FluentValidation
         [Parameter] public IValidator Validator { get; set; }
         [Parameter] public bool DisableAssemblyScanning { get; set; }
 
-        internal Action<ValidationStrategy<object>> Options;
+        [Parameter] public Action<ValidationStrategy<object>> Options { get; set; }
 
-        public bool Validate(Action<ValidationStrategy<object>> options)
+        public bool Validate(Action<ValidationStrategy<object>> options = null)
         {
-            Options = options;
+            if (options != null)
+                Options = options;
 
             try
             {
@@ -27,18 +28,17 @@ namespace Blazored.FluentValidation
             }
             finally
             {
-                Options = null;
+                if (options != null)
+                    Options = null;
             }
         }
 
         protected override void OnInitialized()
         {
             if (CurrentEditContext == null)
-            {
                 throw new InvalidOperationException($"{nameof(FluentValidationValidator)} requires a cascading " +
-                    $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(FluentValidationValidator)} " +
-                    $"inside an {nameof(EditForm)}.");
-            }
+                                                    $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(FluentValidationValidator)} " +
+                                                    $"inside an {nameof(EditForm)}.");
 
             CurrentEditContext.AddFluentValidation(ServiceProvider, DisableAssemblyScanning, Validator, this);
         }
